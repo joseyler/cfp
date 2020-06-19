6844ec7363695cac26446e954764cf427e71f76e
Nahuel <roman.n7978@gmail.com>
2020-06-19 20:32:57 -0300

    Implementacion de la API de cursos

--- cursos/src/main/java/ar/gob/cfp/cursos/dao/CursosRepository.java
+++ cursos/src/main/java/ar/gob/cfp/cursos/dao/CursosRepository.java
@@ -1,11 +1,17 @@
 package ar.gob.cfp.cursos.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ar.gob.cfp.cursos.dao.entities.CursoEntity;
 
+
 public interface CursosRepository extends CrudRepository<CursoEntity, Integer> {
 	
-	
+ List<CursoEntity> findByIdInstitucion(Integer IdInstitucion);
+ 
+ Optional<CursoEntity> findById(Integer id);
 
 }
--- cursos/src/main/java/ar/gob/cfp/cursos/dao/mappers/CursosMapper.java
+++ cursos/src/main/java/ar/gob/cfp/cursos/dao/mappers/CursosMapper.java
@@ -1,6 +1,10 @@
 package ar.gob.cfp.cursos.dao.mappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ar.gob.cfp.commons.model.Curso;
+import ar.gob.cfp.commons.model.Profesor;
 import ar.gob.cfp.cursos.dao.entities.CursoEntity;
 
 public class CursosMapper {
@@ -13,7 +17,29 @@ public class CursosMapper {
 		ce.setNombre(cursoModel.getNombre());
 		ce.setPeriodoAcademico(cursoModel.getPeriodoAcademico());
 		ce.setIdInstitucion(cursoModel.getInstitucion().getId());
+		ce.setIdProfesor(cursoModel.getProfesor().getId());
 		return ce;
+	}
+
+	public static List<Curso> mapModel(List<CursoEntity> cursos) {
+		List<Curso> respuesta = new ArrayList<Curso>();
+		for(CursoEntity indice : cursos) {
+			respuesta.add(mapp(indice));
+		}
+		
+		return respuesta;
+	}
+
+	public static Curso mapp(CursoEntity indice) {
+		 Curso r = new Curso();
+		 r.setId(indice.getId());
+		 r.setNombre(indice.getNombre());
+		 r.setHorasCatedra(indice.getHorasCatedra());
+		 r.setFechaInicio(indice.getFechaInicio());
+		 r.setPeriodoAcademico(indice.getPeriodoAcademico());
+		 r.setProfesor(new Profesor());
+		 r.getProfesor().setId(indice.getIdProfesor());
+		return r;
 	}
 
 }
