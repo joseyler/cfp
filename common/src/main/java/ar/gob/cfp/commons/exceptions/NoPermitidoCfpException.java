a29b01bfd224d99274455024dd23fb0c172a24f5
Jose Eyler <joseyler@gmail.com>
2020-06-25 21:05:19 -0300

    Jerarquia de Excepciones

---
+++ common/src/main/java/ar/gob/cfp/commons/exceptions/NoPermitidoCfpException.java
@@ -0,0 +1,17 @@
+package ar.gob.cfp.commons.exceptions;
+
+public class NoPermitidoCfpException extends CfpException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public NoPermitidoCfpException(String message) {
+        super(message);
+    }
+    
+    
+    
+
+}
--- personal/src/main/java/ar/gob/cfp/personal/dao/ProfesorDao.java
+++ personal/src/main/java/ar/gob/cfp/personal/dao/ProfesorDao.java
@@ -1,58 +1,87 @@
 package ar.gob.cfp.personal.dao;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.gob.cfp.commons.exceptions.CfpException;
+import ar.gob.cfp.commons.exceptions.ObjetoNoEncontradoCfpException;
+import ar.gob.cfp.commons.exceptions.RecursoExistenteCfpException;
 import ar.gob.cfp.commons.model.Profesor;
 import ar.gob.cfp.personal.dao.entities.ProfesorEntity;
 import ar.gob.cfp.personal.dao.mappers.ProfesorMapper;
 
 @Service
-public class ProfesorDao  {
-	
-	@Autowired
-	ProfesorRepository repo;
+public class ProfesorDao {
 
-//	public List<Profesor> getAll2() {
-//		EntityManager em = emf.createEntityManager();
-//		try {
-//			TypedQuery<ProfesorEntity> query = em.createQuery("select d from ProfesorEntity d ",ProfesorEntity.class);
-//			List<ProfesorEntity> resultList = query.getResultList();
-//			return ProfesorMapper.mapModelo(resultList);
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			em.close();
-//		}
-//		return new ArrayList<Profesor>();
-//	}
-	
-	
-	public List<Profesor> getAll() {
-		List<ProfesorEntity> resultList = repo.findAll();
-		return ProfesorMapper.mapModelo(resultList);
-	}
+    @Autowired
+    ProfesorRepository repo;
 
-	
-	public Profesor guardarProfesor(Profesor prof) {
-		ProfesorEntity ent = ProfesorMapper.mapEntity(prof);
-		repo.save(ent);
-		prof.setId(ent.getId());
-		return prof;
-	}
+    // public List<Profesor> getAll2() {
+    // EntityManager em = emf.createEntityManager();
+    // try {
+    // TypedQuery<ProfesorEntity> query = em.createQuery("select d from ProfesorEntity d ",ProfesorEntity.class);
+    // List<ProfesorEntity> resultList = query.getResultList();
+    // return ProfesorMapper.mapModelo(resultList);
+    // }catch (Exception e) {
+    // e.printStackTrace();
+    // } finally {
+    // em.close();
+    // }
+    // return new ArrayList<Profesor>();
+    // }
 
-	public Profesor getProfesorById(Integer idProfesor) {
-		Optional<ProfesorEntity> entOp = this.repo.findById(idProfesor);
-		ProfesorEntity ProfesorEntity = entOp.get();
-		return ProfesorMapper.mapProfesorModelo(ProfesorEntity);
-	}
+    public List<Profesor> getAll() {
+        List<ProfesorEntity> resultList = repo.findAll();
+        return ProfesorMapper.mapModelo(resultList);
+    }
 
-	
-	
-	
-	
-	
+    public Profesor guardarProfesor(Profesor prof) throws CfpException {
+        List<ProfesorEntity> profesoresDelDni = repo.findByDni(prof.getDni());
+        if (profesoresDelDni.isEmpty()) {
+            ProfesorEntity ent = ProfesorMapper.mapEntity(prof);
+            repo.save(ent);
+            prof.setId(ent.getId());
+            return prof;
+        } else {
+            throw new RecursoExistenteCfpException("Ya existe un profesor para el dni " + prof.getDni());
+        }
+
+    }
+
+    public Profesor getProfesorById(Integer idProfesor) throws CfpException {
+        try {
+            Optional<ProfesorEntity> entOp = this.repo.findById(idProfesor);
+            ProfesorEntity profesorEntity = entOp.get();
+            return ProfesorMapper.mapProfesorModelo(profesorEntity);
+        } catch (NoSuchElementException e) {
+            throw new ObjetoNoEncontradoCfpException("No se encuentra profesor para el id: " + idProfesor);
+        }
+    }
+
+    public Profesor actualizarProfesor(Profesor profesor) throws CfpException {
+        try {
+            Optional<ProfesorEntity> entOp = this.repo.findById(profesor.getId());
+            ProfesorEntity profesorEntity = entOp.get();
+            ProfesorEntity ent = ProfesorMapper.mapEntity(profesor);
+            repo.save(ent);
+            return profesor;
+        } catch (NoSuchElementException e) {
+            throw new ObjetoNoEncontradoCfpException("No se encuentra profesor para el id: " + profesor.getId());
+        }
+    }
+
+    public void eliminarProfesor(Integer idProfesor) throws CfpException {
+        try {
+            Optional<ProfesorEntity> entOp = this.repo.findById(idProfesor);
+            ProfesorEntity profesorEntity = entOp.get();
+            this.repo.delete(profesorEntity);
+        } catch (NoSuchElementException e) {
+            throw new ObjetoNoEncontradoCfpException("No se encuentra profesor para el id: " + idProfesor);
+        }
+    }
+
 }
