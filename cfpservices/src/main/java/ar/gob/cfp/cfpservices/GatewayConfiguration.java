e3aa55eef20bc3689bd5770edd9bddeda0b7a85a
Jose Eyler <joseyler@gmail.com>
2020-07-17 20:39:04 -0300

    Eureka Cliente

--- cfpservices/src/main/java/ar/gob/cfp/cfpservices/GatewayConfiguration.java
+++ cfpservices/src/main/java/ar/gob/cfp/cfpservices/GatewayConfiguration.java
@@ -1,10 +1,13 @@
 package ar.gob.cfp.cfpservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.netflix.discovery.EurekaClient;
 
 import ar.gob.cfp.cfpservices.filter.AutorizationFilter;
 
@@ -12,14 +15,15 @@ import ar.gob.cfp.cfpservices.filter.AutorizationFilter;
 @Configuration
 public class GatewayConfiguration {
     
+    @Autowired
+    EurekaClient eurekaClient;
+    
     @Bean
     public RouteLocator misRutas(RouteLocatorBuilder builder) {
         return builder.routes().
             route(p -> p
                     .path("/personal/**")
-                    .filters(f -> f.addRequestHeader("Ruteado", "personal")
-                            .addResponseHeader("Routeado", "personal")
-                            .filter(new AutorizationFilter())
+                    .filters(f -> f.filter(new AutorizationFilter(eurekaClient))
 //                            .hystrix(config -> config.setName("personal")
 //                                                .setFallbackUri("forward:/personalfallback")))
                     ).uri("lb://personal")
@@ -28,7 +32,7 @@ public class GatewayConfiguration {
                     .path("/distritales/**")
                     .filters(f -> f.addRequestHeader("Routeado", "distritales")
                           .addResponseHeader("Routeado", "personal")
-                          .filter(new AutorizationFilter())
+                          .filter(new AutorizationFilter(eurekaClient))
 //                            .hystrix(config -> config.setName("distritales")
 //                                                .setFallbackUri("forward:/distritalesfallback"))
                             )
@@ -36,21 +40,21 @@ public class GatewayConfiguration {
             .route (p -> p
                     .path("/cursos/**")
                     .filters(f -> f
-                            .filter(new AutorizationFilter())
+                            .filter(new AutorizationFilter(eurekaClient))
                             .hystrix(config -> config.setName("cursos")
                                                 .setFallbackUri("forward:/cursosfallback")))
-                    .uri("http://localhost:8073/cursos"))
+                    .uri("lb://cursos"))
             .route (p -> p
                     .path("/inscripciones/**")
                     .filters(f -> f
-                            .filter(new AutorizationFilter())
+                            .filter(new AutorizationFilter(eurekaClient))
                             .hystrix(config -> config.setName("inscripciones")
                                                 .setFallbackUri("forward:/inscripcionesfallback")))
-                    .uri("http://localhost:8071/inscripciones"))
+                    .uri("lb://inscripciones"))
             .route (p -> p
                     .path("/autorizaciones/**")
                     .filters(f -> f
-                            .filter(new AutorizationFilter())
+                            .filter(new AutorizationFilter(eurekaClient))
 //                            .hystrix(config -> config.setName("autorizaciones")
 //                                                .setFallbackUri("forward:/autorizacionesfallback"))
                             )
