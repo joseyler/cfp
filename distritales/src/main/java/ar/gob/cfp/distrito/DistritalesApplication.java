f8c8e919f82ad4cfb7b43404b533864eee2347ad
Jose Eyler <joseyler@gmail.com>
2020-07-17 19:50:58 -0300

    Eureka

--- distritales/src/main/java/ar/gob/cfp/distrito/DistritalesApplication.java
+++ distritales/src/main/java/ar/gob/cfp/distrito/DistritalesApplication.java
@@ -3,12 +3,16 @@ package ar.gob.cfp.distrito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import ar.gob.cfp.commons.jsonmap.ObjectMapperProvider;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 @EnableAutoConfiguration(exclude = { })
 public class DistritalesApplication {
 
